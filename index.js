cd447b1d2aebbab538ccff65d5914b9bb7ace960
GabrielBatistaNunes <142436040+GabrielBatistaNunes@users.noreply.github.com>
2023-08-28 17:13:07 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+// let nome = prompt("Seu nome")
+// let cor = prompt("Qual sua cor favorita?")
+
+//jeitos de criar uma string
+// console.log("A cor favorita de " + nome + " é " + cor);
+// console.log(`A cor favorita de ${nome} é ${cor}`)
+
+//verifica o número de letras no texto
+// console.log(nome.length);
+
+//transforma em maiuscula ou minuscula
+// console.log(nome.toLowerCase());
+// console.log(nome.toUpperCase());
+
+//retira os espaços ao redor, mas não dentro da string
+// console.log(nome.trim());
+
+//método para buscar determinado texto dentro da string
+// console.log(nome.includes("cenoura"));
+
+//substitui uma determinada palavra por outra
+// console.log(nome.replaceAll("cenoura", "batata"));
+
+// let texto = prompt("Escreva qualquer coisa");
+// console.log(texto.toUpperCase());
+// console.log(texto.replaceAll("o", "i"));
+// console.log(texto.length);
+
+//arrays
+//o primeiro valor é 0, dps 1, 2, etc.
+// let listaDeExercicios = ["elevacao lateral", "supino", "voador"];
+// console.log(listaDeExercicios[0]);
+// console.log(listaDeExercicios[1]);
+// console.log(listaDeExercicios[2]);
+// console.log(listaDeExercicios);
+
+//exercicio
+// //let raças = ["poodle","pug", "hotweelis","bulldog", "akita"];
+// //console.log(raças[n = Number(prompt("escolha um número de 0 a 4 para ver uma raça de cachorro"))], "-"+n);
+//let escolha = prompt("escolha um número de 0 a 4 para ver uma raça de cachorro");
+//console.log(raças[escolha]);
+
+// const pokemon = ["bulbassauro", "squirtle", "charmander"];
+// console.log(pokemon.length);
+// console.log(pokemon[0].length);
+
+// const seriesboas = ["Breaking Bad", "Brooklyn Nine-nine"];
+// console.log(seriesboas.includes("Breaking Bad"));
+// console.log(seriesboas.includes ("Game of Thrones"));
+
+// let instrumentos = ["violao", "guitarra", "bateria"];
+// instrumentos.push("teclado");
+// console.log(instrumentos);
+// instrumentos.pop();
+// console.log(instrumentos);
+
+// instrumentos.splice(1, 2);
+// console.log(instrumentos);
+
+//Exercicio
+
+n = ["1","2","3","4","5", "6"];
+n.push("7");
+n.splice(3, 2)
+console.log(n.lenght);
+console.log(n);
